849793201633cb0a906df62841ae4512e75a552e
Isojay <isongum@gmail.com>
2023-08-27 17:00:24 +0545

    minor changes

--- Micro_LibrarySystem/StudentServices/src/main/java/com/example/StudentServices/Controller/UserController.java
+++ Micro_LibrarySystem/StudentServices/src/main/java/com/example/StudentServices/Controller/UserController.java
@@ -1,11 +1,14 @@
 package com.example.StudentServices.Controller;
 
+import com.example.StudentServices.DTO.UserDTO;
 import com.example.StudentServices.DTO.UserResponse;
 import com.example.StudentServices.Service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,9 +21,29 @@ public class UserController {
     public List<UserResponse> findAll(){
         return userService.findAll();
     }
+
+    @PostMapping
+    public void addStudent(@RequestBody UserDTO userDTO){
+        userService.save(userDTO);
+    }
+
+    @GetMapping("/findByid/{id}")
+    public ResponseEntity<?> findByid(@PathVariable String id){
+        Optional<UserResponse> user = userService.findById(id);
+        if (user.isPresent()){
+            return ResponseEntity.ok(user);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
     @PostMapping("/{id}")
     public void enableIt(@PathVariable String id){
         userService.enableId(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteIt(@PathVariable String id){
+        userService.deleteById(id);
     }
 
 }
